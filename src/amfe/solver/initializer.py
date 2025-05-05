229d8f29b4f9aade46ad706c1bc12517c905e39d
Christian Meyer <christian.meyer@tum.de>
2025-05-05 09:51:38 +0200

    [Bug] - Fix bug in acceleration initializer for time integration solvers

--- src/amfe/solver/initializer.py
+++ src/amfe/solver/initializer.py
@@ -32,5 +32,5 @@ class LinearAccelerationInitializer:
 
     def get_acceleration(self, t0, q0, dq0):
         A = self.M(q0, dq0, t0)
-        b = self.f_ext(q0, dq0, t0) - self.D(q0, dq0, t0) @ dq0 - self.f_int(q0, dq0, t0)
+        b = self.f_ext(q0, dq0, t0) - self.f_int(q0, dq0, t0)
         return self.solve_function(A, b, **self.solve_function_kwargs)
